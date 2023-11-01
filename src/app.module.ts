cc08e40d78b830a386d7ed6fa9dcbb808712756b
developersomin <angiwon12345@nate.com>
2023-11-02 00:14:05 +0900

    init: 초기 프로젝트 설정

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { EvaluationsModule } from './evaluations/evaluations.module';
+import { AreaModule } from './area/area.module';
+import { OpenApiModule } from './openapi/open-api.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+    UsersModule,
+    RestaurantsModule,
+    EvaluationsModule,
+    AreaModule,
+    OpenApiModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
