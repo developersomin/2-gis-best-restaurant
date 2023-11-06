898b30040ad83a991153087552d91282c821e9d9
developersomin <angiwon12345@nate.com>
2023-11-06 23:07:49 +0900

    feat: 시군구 목록 API
    레스토랑 컨트롤러 작성
    
    feat-#26

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
+import { Restaurants } from './entities/restaurants.entity';
+import { PaginateRestaurantDto } from './dto/paginate-restaurant.dto';
+import { IFindByDosiAndSgg } from './interface/restaurants-service.interface';
+import { GetResDto } from './dto/get-restaurants.dto';
 
 @Controller('restaurants')
 export class RestaurantsController {
-  constructor(private readonly restaurantsService: RestaurantsService) {}
+	constructor(private readonly restaurantsService: RestaurantsService) {}
+
+	@Get('/sortBy')
+	findByDosiSgg(@Query() paginateRestaurantDto: PaginateRestaurantDto): Promise<IFindByDosiAndSgg> {
+		return this.restaurantsService.findByDosiOrSgg(paginateRestaurantDto);
+	}
+
+	@Get()
+	getRes(@Query() getResDto: GetResDto) {
+		return this.restaurantsService.getRes(getResDto);
+	}
 }
