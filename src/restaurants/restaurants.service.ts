2ae898cdcdd6bfcd995e21aee3d77b10c66e3227
developersomin <angiwon12345@nate.com>
2023-11-06 23:06:45 +0900

    fix: 시군구 목록 API
    맛집 목록 api 작성
    
    feat-#26

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,9 @@
 import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurants } from './entities/restaurants.entity';
-import { Repository } from 'typeorm';
+import { Between, MoreThan, Repository } from 'typeorm';
 import { OpenApiService } from '../openapi/open-api.service';
+import { IFindByDosiAndSgg } from './interface/restaurants-service.interface';
 
 @Injectable()
 export class RestaurantsService implements OnModuleInit {
@@ -27,7 +28,7 @@ export class RestaurantsService implements OnModuleInit {
 					roadAddr: row.REFINE_ROADNM_ADDR,
 					lotNoAddr: row.REFINE_LOTNO_ADDR,
 					zipNo: row.REFINE_ZIPNO,
-					lot: row.REFINE_WGS84_LOGT,
+					lon: row.REFINE_WGS84_LOGT,
 					lat: row.REFINE_WGS84_LAT,
 					area: {
 						dosi: row.SIDO_NM,
@@ -48,6 +49,58 @@ export class RestaurantsService implements OnModuleInit {
 				lotNoAddr: restaurants.lotNoAddr,
 			},
 		});
+	}
+	async findByDosiOrSgg(paginateRestaurantDto): Promise<IFindByDosiAndSgg> {
+		const [restaurants, total] = await this.restaurantsRepository.findAndCount({
+			where: {
+				area: {
+					dosi: paginateRestaurantDto.dosi,
+					sgg: paginateRestaurantDto.sgg,
+				},
+				resNo: MoreThan(paginateRestaurantDto.cursor ?? 0),
+			},
+			relations: ['area'],
+			take: paginateRestaurantDto.take,
+		});
+		const lastItem =
+			restaurants.length > 0 && restaurants.length === paginateRestaurantDto.take
+				? restaurants[restaurants.length - 1]
+				: null;
+		const nextUrl = lastItem && new URL('http://localhost:3000/restaurants');
+		if (nextUrl) {
+			for (const key of Object.keys(paginateRestaurantDto)) {
+				if (key !== 'cursor') {
+					nextUrl.searchParams.append(key, paginateRestaurantDto[key]);
+				}
+			}
+			nextUrl.searchParams.append('cursor', lastItem.resNo.toString());
+		}
+
+		return {
+			data: restaurants,
+			total,
+			cursor: {
+				after: lastItem?.resNo ?? null,
+			},
+			count: restaurants.length,
+			next: decodeURIComponent(nextUrl?.toString() ?? null),
+		};
+	}
+
+	async getRes(getResDto) {
+		const qb = this.restaurantsRepository.createQueryBuilder('restaurants');
+		qb.addSelect(
+			`6371 * acos(cos(radians(${getResDto.lat})) * cos(radians(lat)) * cos(radians(lon) - radians(${getResDto.lon})) + sin(radians(${getResDto.lat})) * sin(radians(lat)))`,
+			'distance',
+		);
+		if (getResDto.order__distance) {
+			qb.orderBy('distance', getResDto.order__distance);
+		} else if (getResDto.order__scoreAvg) {
+			qb.orderBy('scoreAvg', getResDto.order__scoreAvg);
+		}
+		qb.having(`distance <= ${getResDto.range}`);
+		const result = await qb.getRawMany();
+		return result;
 	}
 
 	async updateRes(restaurants: Pick<Restaurants, 'resName' | 'lotNoAddr' | 'scoreAvg'>): Promise<Restaurants> {
